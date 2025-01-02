8c498ebdfd055ea4469c25bf9307e58a46fa7054
alpaca34607 <alpaca34607@gmail.com>
2025-01-02 19:05:29 +0800

    0102

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+const basename =
+  process.env.NODE_ENV === 'production' ? '/Mystic-Markers' : '/';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
-)
+);
