84a0a3feb0330048fa483533a72d3e6a9974689a
Alpaca_lu <alpaca34607@gmail.com>
2026-02-23 04:35:20 +0800

    Add Firebase configuration to .env file and update firebase.jsx to use environment variables

--- src/config/firebase.jsx
+++ src/config/firebase.jsx
@@ -7,12 +7,12 @@ import { initializeApp } from "firebase/app";
 import{getAuth, GoogleAuthProvider} from "firebase/auth"
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBJpVoNmPTlHhwbGV01LK3k27--Km6dEVA",
-  authDomain: "mystic-markers.firebaseapp.com",
-  projectId: "mystic-markers",
-  storageBucket: "mystic-markers.firebasestorage.app",
-  messagingSenderId: "705157300195",
-  appId: "1:705157300195:web:438c37496f807d26b3ead6"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
