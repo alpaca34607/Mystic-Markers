bbd9c9e6fdef6975f6c9474f1617ccd1d0492f3b
Alpaca_lu <alpaca34607@gmail.com>
2026-02-23 03:30:59 +0800

    Revert "Refactor Vite configuration to dynamically set base path based on environment mode and add new package.json for module type"
    This reverts commit 1fb84ad7b64b8d341c0f0bc0945f983bc1c448d6.

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/Mystic-Markers/' : '/',
+export default defineConfig({
+  // base:process.env.NODE_ENV === 'production'?'/Mystic-Markers/':'/',
+  base:'./',
 
   plugins: [react()],
 
@@ -14,4 +15,4 @@ export default defineConfig(({ mode }) => ({
       },
     },
   }
-}))
+})
